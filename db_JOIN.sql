24af13642ba9baf6f79ae34be528e96dc9d9af38
AngeloCrisci <angelocrisci1998@gmail.com>
2024-10-17 17:37:59 +0200

    3. JOIN done

--- db_JOIN.sql
+++ db_JOIN.sql
@@ -15,3 +15,13 @@ FROM degrees
 JOIN departments ON degrees.department_id = departments.id 
 WHERE degrees.level = 'Magistrale'
 AND departments.name LIKE '%Neuroscienze';
+
+
+-- Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+SELECT courses.name AS 'Nome_corso', 
+        teachers.name AS 'Nome insegnante', 
+            teachers.surname AS 'Cognome Insegnante' 
+FROM courses 
+JOIN course_teacher ON courses.id = course_teacher.course_id 
+JOIN teachers ON course_teacher.teacher_id = teachers.id 
+WHERE teachers.id = 44;
